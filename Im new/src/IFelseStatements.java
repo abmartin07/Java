d56598ecc8d3da7fca0e61ee60c149dea5dcd072
abmartin07 <abmartin07@gmail.com>
2016-11-12 21:01:34 -0600

    added the if and else statements

--- Im new/src/IFelseStatements.java
+++ Im new/src/IFelseStatements.java
@@ -3,13 +3,20 @@ public class IFelseStatements {
 
 	public static void main(String[] args) {
 	
+		int i = 11;
+		if(i<10)
+			System.out.println(i+" is less than 10");
 		
-		if(true){
+		else if(i==10)
+			System.out.println(i+" is equal to 10");
 			
-			
+		else
+			System.out.println(i+" is greater than 10");
+				
+				
 		}
 		
 		
 	}
 
-}
+
