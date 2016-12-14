616c11a66b5a49f6790d16a9d7e65bef5f19ae2f
abmartin07 <abmartin07@gmail.com>
2016-12-14 12:31:07 -0600

    Excel read

---
+++ ReadExcel/src/ExcelReadandWrite/ReadExcel.java
@@ -0,0 +1,13 @@
+package ExcelReadandWrite;
+
+import java.io.File;
+
+public class ReadExcel {
+
+	public static void main(String[] args) {
+		
+		File src = new File("B:\\Docs\\Saibersys\\5211_weekly_Status.xlsx");
+
+	}
+
+}
